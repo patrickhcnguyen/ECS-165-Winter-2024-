ce4cd8b421a188132114d648a72678ff35c87b34
Grace Chen <gracechen56607@gmail.com>
2024-02-13 23:22:07 -0800

    error

--- query.py
+++ query.py
@@ -51,7 +51,7 @@ class Query:
     def insert(self, *columns):
         primary_key = columns[self.table.key-4]
         rid = self.table.index.locate(self.table.key, primary_key)
-        if rid is None:
+        if rid == []:
             self.table.insert_record(*columns)  # if primary key not found
     
     """
@@ -78,7 +78,7 @@ class Query:
     # Assume that select will never be called on a key that doesn't exist
     """
     def select_version(self, search_key, search_key_index, projected_columns_index, relative_version):
-        pass
+        self.table.select_record(search_key, search_key_index, projected_columns_index)
 
     
     """
